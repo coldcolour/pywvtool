dae602f986e20e3b29ecf32ab1d052bb691c052e
Huang Song <huangsong@gmail.com>
2014-08-16 22:52:33 +0800

    refresh a new version of mmseg; build _mmseg.so from mmseg-cpp folder on other platform

--- mmseg/__init__.py
+++ mmseg/__init__.py
@@ -1,8 +1,27 @@
-#coding:utf-8
-
+#encoding:utf8
+import os
+from _mmseg import Dictionary as _Dictionary, Token, Algorithm
 import _mmseg as mmseg
-mmseg.dict_load_defaults()
 
+class Dictionary(_Dictionary):
+    dictionaries = (
+        ('chars', os.path.join(os.path.dirname(__file__), 'data', 'chars.dic')),
+        ('words', os.path.join(os.path.dirname(__file__), 'data', 'words.dic')),
+    )
+
+    @staticmethod
+    def load_dictionaries():
+        for t, d in Dictionary.dictionaries:
+            if t == 'chars':
+                if not Dictionary.load_chars(d):
+                    raise IOError("Cannot open '%s'" % d)
+            elif t == 'words':
+                if not Dictionary.load_words(d):
+                    raise IOError("Cannot open '%s'" % d)
+
+dict_load_defaults = Dictionary.load_dictionaries
+dict_load_defaults()
+ 
 def seg_txt(text):
     if type(text) is str:
         algor = mmseg.Algorithm(text)
